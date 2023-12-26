70ddf3a25037515d73a5d766b512815fce89a03b
radek.Bednarik <radek.bednarik@tesena.com>
2023-12-26 18:30:10 +0100

    dev: removed some stuff, wip

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { cli } from "./cli/cli.js";
 
 (async () => {
-  const parsed = await cli.parseAsync(process.argv);
-  console.log("parsed", parsed.args);
+  await cli.parseAsync(process.argv);
 })();
