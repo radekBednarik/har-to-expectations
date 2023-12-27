3a3599688a0b8b83c6360a32810358d80c271691
radek.Bednarik <radek.bednarik@tesena.com>
2023-12-27 08:15:45 +0100

    dev: fixed error strings; added write json func

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,9 @@ import { cli } from "./cli/cli.js";
 
 (async () => {
   try {
-    
     await cli.parseAsync(process.argv);
-
   } catch (err: any) {
     console.error("program returned error: ${err}");
-    process.exit(1);    
+    process.exit(1);
   }
 })();
--- src/io/io.ts
+++ src/io/io.ts
@@ -1,4 +1,4 @@
-import { readFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 import { resolve } from "path";
 import { Har } from "har-format";
 
@@ -6,9 +6,20 @@ export async function readHarFile(filepath: string) {
   const fullPath = resolve(filepath);
 
   try {
-    return JSON.parse((await readFile(fullPath, { encoding: "utf-8" }))) as Har;
+    return JSON.parse(await readFile(fullPath, { encoding: "utf-8" })) as Har;
   } catch (err: any) {
-    console.error("func readHarFile returned error: ${err}");
+    console.error(`func readHarFile returned error: ${err}`);
+    throw err;
+  }
+}
+
+export async function writeJsonFile(data: any, filepath: string) {
+  const fullPath = resolve(filepath);
+
+  try {
+    await writeFile(fullPath, JSON.stringify(data, null, 2));
+  } catch (err: any) {
+    console.error(`func writeJsonFile returned error: ${err}`);
     throw err;
   }
 }
