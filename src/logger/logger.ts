f51e4adc5705135c639cc15f72594eeb0f27bd35
radek.Bednarik <radek.bednarik@tesena.com>
2023-12-28 15:40:55 +0100

    dev: added logger module

---
+++ src/logger/logger.ts
@@ -0,0 +1,18 @@
+import { pino } from "pino";
+
+const transport = pino.transport({
+  targets: [
+    {
+      target: "pino-pretty",
+      options: {
+        level: process.env["LOG_LEVEL"] ? process.env["LOG_LEVEL"] : "info",
+      },
+    },
+  ],
+});
+
+export const logger = pino({
+  transport,
+  level: process.env["LOG_LEVEL"] ? process.env["LOG_LEVEL"] : "info",
+  enabled: process.env["LOG_ENABLED"] == "true" ? true : false,
+});
